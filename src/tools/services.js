862bebb3e1692b159f0b5b08469a8d6423514350
Sholto <madein.mania@gmail.com>
2021-11-05 11:13:57 +0000

    wrap functions in IIFE

--- src/tools/services.js
+++ src/tools/services.js
@@ -1,68 +1,70 @@
-const handleErrors = (res) => {
-  if (!res.ok) throw res;
-  return res.json();
-};
-
-const fetchAll = (url) => {
-  const options = {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
+export const Server = (() => {
+  const handleErrors = (res) => {
+    if (!res.ok) throw res;
+    return res.json();
   };
-  return fetch(url, options)
-    .then((res) => res.json())
-    .then((data) => data)
-    .catch((err) => console.log(err));
-};
 
-const fetchPost = (url) => {
-  const options = {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json' },
+  const fetchAll = (url) => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    return fetch(url, options)
+      .then((res) => res.json())
+      .then((data) => data)
+      .catch((err) => console.log(err));
   };
-  return fetch(url, options)
-    .then((data) => data)
-    .catch((err) => console.log(err));
-};
 
-const create = (url, newObject) => {
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(newObject),
+  const fetchPost = (url) => {
+    const options = {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    };
+    return fetch(url, options)
+      .then((data) => data)
+      .catch((err) => console.log(err));
   };
-  return fetch(url, options)
-    .then(handleErrors)
-    .then((data) => data);
-};
 
-const update = (url, newObject) => {
-  const options = {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(newObject),
+  const create = (url, newObject) => {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newObject),
+    };
+    return fetch(url, options)
+      .then(handleErrors)
+      .then((data) => data);
   };
-  return fetch(url, options)
-    .then(handleErrors)
-    .then((data) => data);
-};
 
-const remove = (url) => {
-  const options = {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-    },
+  const update = (url, newObject) => {
+    const options = {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newObject),
+    };
+    return fetch(url, options)
+      .then(handleErrors)
+      .then((data) => data);
   };
-  return fetch(url, options)
-    .then((res) => res.json())
-    .then((data) => data)
-    .catch((err) => console.log(err));
-};
 
-export default { fetchAll, fetchPost, create, update, remove };
+  const remove = (url) => {
+    const options = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    return fetch(url, options)
+      .then((res) => res.json())
+      .then((data) => data)
+      .catch((err) => console.log(err));
+  };
+
+  return { fetchAll, fetchPost, create, update, remove };
+})();
