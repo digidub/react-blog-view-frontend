0dd423a3f29d209fe2c010a560df702f8013646a
Sholto <madein.mania@gmail.com>
2021-11-05 11:21:15 +0000

    fetch and map display of posts

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Fragment } from 'react/cjs/react.production.min';
+import { Server } from '../tools/services';
+import PostsListCard from './PostsListCard';
 
 const PostsList = () => {
-  return <div>list of posts to be rendered here</div>;
+  const [posts, setPosts] = useState(null);
+  const url = '/posts';
+
+  useEffect(() => {
+    Server.fetchAll(url).then((data) => {
+      console.log(data);
+      setPosts(data);
+    });
+  }, []);
+
+  const listOfPosts = () =>
+    posts.map((post) => (
+      <PostsListCard
+        key={post._id}
+        title={post.title}
+        author={post.author.username}
+        posted={post.datePosted}
+      />
+    ));
+
+  return <Fragment>{posts && listOfPosts()}</Fragment>;
 };
 
 export default PostsList;
