7caff71c1325514c8b4bec3282b2f8223583c848
Sholto <madein.mania@gmail.com>
2021-11-15 08:24:23 +0000

    styling

--- src/components/PostsListCard.jsx
+++ src/components/PostsListCard.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const PostsListCard = ({ title, author, posted }) => {
   return (
     <div>
-      <div>{title}</div>
-      <div>{author}</div>
+      <CardHeader>
+        <div>{title}</div>
+        <div>{author}</div>
+      </CardHeader>
       <div>{posted}</div>
     </div>
   );
 };
 
 export default PostsListCard;
+
+const CardHeader = styled.div`
+  display: flex;
+`;
